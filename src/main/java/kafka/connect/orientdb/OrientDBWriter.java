c16f43e0fba20465cb62d536e2d464848bc7ae75
Thomas <sathomas@ct1-8780-m.home>
2018-02-03 23:05:20 -0500

    adding the writter

--- src/main/java/kafka/connect/orientdb/OrientDBWriter.java
+++ src/main/java/kafka/connect/orientdb/OrientDBWriter.java
@@ -3,6 +3,7 @@ package kafka.connect.orientdb;
 import java.util.Collection;
 import java.util.Map;
 
+import org.apache.kafka.connect.errors.RetriableException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,49 +13,48 @@ import com.orientechnologies.orient.core.record.impl.ODocument;
 
 import kafka.connect.orientdb.sink.OrientDBSinkConfig;
 
-
 /**
  * 
  * @author Sanju Thomas
  *
  */
-public class OrientDBWriter implements Writer{
-	
+public class OrientDBWriter implements Writer {
+
 	private static final Logger logger = LoggerFactory.getLogger(OrientDBWriter.class);
-	
-	private final String host;
-	private final String dbName;
+
+	private final String connectionString;
 	private final String username;
 	private final String password;
-	
-	
-	public OrientDBWriter(final Map<String, String> config){
-	    host = config.get(OrientDBSinkConfig.ORIENTDB_HOST);
-	    dbName = config.get(OrientDBSinkConfig.DATABASE_NAME);
-	    username = config.get(OrientDBSinkConfig.CONNECTION_USER);
-	    password = config.get(OrientDBSinkConfig.CONNECTION_PASSWORD);
+
+	public OrientDBWriter(final Map<String, String> config) {
+		connectionString = config.get(OrientDBSinkConfig.CONNECTION_STRING);
+		username = config.get(OrientDBSinkConfig.CONNECTION_USER);
+		password = config.get(OrientDBSinkConfig.CONNECTION_PASSWORD);
 	}
-	
 
 	@SuppressWarnings({ "unchecked", "resource" })
-    @Override
-    public void write(final Collection<SinkRecord> records) {
-		
+	@Override
+	public void write(final Collection<SinkRecord> records) {
+
 		ODatabaseDocumentTx db = null;
 		try {
-				db = new ODatabaseDocumentTx("remote:" + host + "/" +dbName).open(username, password);
-				for(final SinkRecord record : records) {
-					final Map<Object, Object> mapRecord = (Map<Object, Object>) record.value();
-					final ODocument document = new ODocument(record.topic(), mapRecord);
-					db.save(document);
-				}
+			db = new ODatabaseDocumentTx(connectionString).open(username, password);
+			db.begin();
+			for (final SinkRecord record : records) {
+				final Map<Object, Object> mapRecord = (Map<Object, Object>) record.value();
+				final ODocument document = new ODocument(record.topic(), mapRecord);
+				db.save(document);
+			}
+
 		} catch (Exception e) {
+			db.rollback();
 			logger.error(e.getMessage(), e);
-		}finally {
+			throw new RetriableException(e);
+		} finally {
 			if (null != db) {
 				db.commit();
 				db.close();
 			}
 		}
-    }
+	}
 }
--- src/main/java/kafka/connect/orientdb/sink/OrientDBSinkConfig.java
+++ src/main/java/kafka/connect/orientdb/sink/OrientDBSinkConfig.java
@@ -18,17 +18,14 @@ public class OrientDBSinkConfig extends AbstractConfig {
     
     private static final Logger logger = LoggerFactory.getLogger(OrientDBSinkConfig.class);
 	
-	public static final String ORIENTDB_HOST = "orientdb.host";
-	private static final String ORIENTDB_HOST_DOC = "orientdb host ip";
+	public static final String CONNECTION_STRING = "orientdb.connection.string";
+	private static final String CONNECTION_STRING_DOC = "orientdb connection string";
 	
 	public static final String CONNECTION_USER = "orientdb.user";
 	private static final String CONNECTION_USER_DOC = "orientdb connection user.";
 
 	public static final String CONNECTION_PASSWORD = "orientdb.password";
 	private static final String CONNECTION_PASSWORD_DOC = "orientdb connection password";
-	
-	public static final String DATABASE_NAME = "orientdb.database.name";
-	private static final String DATABASE_NAME_DOC = "orientdb database name";
 	
 	public static final String BATCH_SIZE = "orientdb.batch.size";
 	private static final String BATCH_SIZE_DOC = "orientdb batch size";
@@ -44,12 +41,11 @@ public class OrientDBSinkConfig extends AbstractConfig {
 	private static final String RETRY_BACKOFF_MS_DOC = "The time in milliseconds to wait following an error/exception before a retry attempt is made.";
 	
 	public static ConfigDef CONFIG_DEF = new ConfigDef()
-			.define(ORIENTDB_HOST, Type.STRING, Importance.HIGH, ORIENTDB_HOST_DOC)
+			.define(CONNECTION_STRING, Type.STRING, Importance.HIGH, CONNECTION_STRING_DOC)
 			.define(CONNECTION_USER, Type.STRING, Importance.HIGH, CONNECTION_USER_DOC)
 			.define(CONNECTION_PASSWORD, Type.STRING, Importance.LOW, CONNECTION_PASSWORD_DOC)
 			.define(BATCH_SIZE, Type.INT, Importance.MEDIUM, BATCH_SIZE_DOC)
 			.define(MAX_RETRIES, Type.INT, Importance.MEDIUM, MAX_RETRIES_DOC)
-			.define(DATABASE_NAME, Type.STRING, Importance.MEDIUM, DATABASE_NAME_DOC)
 			.define(WRITER_IMPL, Type.STRING, Importance.MEDIUM, WRITER_IMPL_DOC)
 			.define(RETRY_BACKOFF_MS, Type.INT, RETRY_BACKOFF_MS_DEFAULT, Importance.MEDIUM, RETRY_BACKOFF_MS_DOC);
 
